61b12178a77d0f0c050d1f5a6e00c28a80bd2f39
jen <jes089@ucsd.edu>
2019-02-21 10:34:24 -0800

    adding stuff

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,11 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$(".likeBtn").click(clickLikeBtn);
+}
+
+function clickLikeBtn(e){
+	e.preventDefault();
+	ga('create', 'UA-134960053-1','auto');
+	ga('send','event','signup','click');
 }
